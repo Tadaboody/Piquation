794b59abe8c1b8f8de5449e2a10840b1baffe4e4
Tadaboody <tomerpet@gmail.com>
2017-07-06 21:26:55 +0300

    skeleton file added

---
+++ remove_grid.py
@@ -0,0 +1,17 @@
+import numpy
+import cv2
+
+
+def remove_page_grid(image):  # numpy.median is a function
+    median_size = 7
+    return_image = vertical_median_filter(image, median_size)
+    return_image = horizontal_median_filter(return_image, median_size)
+    return return_image
+
+
+def vertical_median_filter(image, size):
+    pass
+
+
+def horizontal_median_filter(image, size):
+    pass
