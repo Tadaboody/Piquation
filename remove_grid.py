3cd1d0df1bd36d8937d4dfa52b57ce45657248bc
CollectorFTB <gilad.david95@gmail.com>
2017-09-30 19:00:54 +0300

    Add files via upload

--- remove_grid.py
+++ remove_grid.py
@@ -1,62 +1,43 @@
 import numpy
 import cv2
+import time
 
 
 def remove_page_grid(image, median_size):  # numpy.median is a function
+    t1 = time.time()
     return_image = horizontal_median_filter(image, median_size)
-    return_image = vertical_median_filter(return_image, median_size)
+    return_image = cv2.transpose(horizontal_median_filter(cv2.transpose(return_image), median_size))
+    t2 = time.time()
+    print "Time it took to run the function: " + str((t2 - t1))
     return return_image
 
 
 def horizontal_median_filter(image, size):
     size = (size - 1) / 2
-    image_copy = list(numpy.zeros((len(image), 3*len(image[0])), numpy.uint8))
 
-    for i, line in enumerate(image_copy[:]):
-        image_copy[i] = list(line)
-
+    image_copy = list(numpy.zeros((len(image), 2*size + len(image[0])), numpy.uint8))
     re_median_arr = numpy.zeros((len(image), len(image[0])), numpy.uint8)
 
-    for i in range(len(image_copy)):
-        for j in range(len(image_copy[i])):
-            image_copy[i][j] = image[i][j % len(image[i])]
+    # left and right padding
+    for i in xrange(len(image_copy)):
+        for j in xrange(size):
+            image_copy[i][j] = image[i][j + len(image[0]) - size]  # left side padding
+            image_copy[i][j + size + len(image[0])] = image[i][j]  # right side padding
 
-    last_value = 0
-    index = len(image[0])
-    for i in range(len(image)):
-        last_value = progress_bar(i, last_value,    float(len(image)))
-        for j in range(index, 2*index):
-            temp = image_copy[i][j - size:j + 1 + size]
-            re_median_arr[i][j - index - 1] = sorted(list(temp))[len(temp) / 2]
-    return re_median_arr
+    # fill middle
+    for i in xrange(len(image_copy)):
+        for j in xrange(len(image[0])):
+            image_copy[i][j+size] = image[i][j]
 
+    size = 2*size + 1  # return size to original median size
 
-def vertical_median_filter(image, size):
-    size = (size - 1) / 2
-    image_copy = numpy.zeros((3 * len(image), len(image[0])), numpy.uint8)
+    for i in xrange(len(image)):
+        for j in xrange(len(image[0])):
+            temp = image_copy[i][j:j + size]  # take the window in size of the original median window size
+            re_median_arr[i][j] = median(temp)  # take median of median window
 
-    for i, line in enumerate(image_copy[:]):
-        image_copy[i] = list(line)
-
-    re_median_arr = numpy.zeros((len(image), len(image[0])), numpy.uint8)
-
-    for i in range(len(image_copy)):
-        for j in range(len(image_copy[i])):
-            image_copy[i][j] = image[i % len(image)][j]
-
-    index = len(image)
-    last_value = 0
-    for i in range(index, 2 * index):
-        last_value = progress_bar(i-index, last_value, float(len(image)))
-        for j in range(len(image_copy[i])):
-            temp = [element[j] for element in image_copy[i - size:i + size + 1]]
-            re_median_arr[i - index - 1][j] = sorted(list(temp))[len(temp) / 2]
-    return re_median_arr
+    return re_median_arr  # return image after
 
 
-def progress_bar(value, last_value, max_value):
-    progress = float(int(1000*float(value)/max_value))/1000
-    if progress != last_value:
-        print 100*progress, '%'
-        return progress
-    return last_value
+def median(array):
+    return sorted(array)[len(array)/2]
